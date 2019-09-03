1ef92d5c1761ebeea5972123ed7faaaea2434bc6
Daniel Keefe <dfk@umn.edu>
2019-09-03 16:19:39 -0500

    added frames per sec display

---
+++ Assets/MinVR/components/FPS.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS : MonoBehaviour
+{
+    public Color textColor = Color.white;
+
+    void OnGUI() {
+        int w = Screen.width;
+        int h = Screen.height;
+        GUIStyle style = new GUIStyle();
+        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 2 / 100;
+        style.normal.textColor = textColor;
+        float fps = 1.0f / Time.smoothDeltaTime;
+        string text = string.Format("{0:0.} fps", fps);
+        GUI.Label(rect, text, style);
+    }
+}
